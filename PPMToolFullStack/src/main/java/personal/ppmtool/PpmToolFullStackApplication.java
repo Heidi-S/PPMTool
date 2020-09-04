710f1dbd73212b76787db952f22d69f7b7886904
Heidi Sheng <heidisheng@MacBook-Air.local>
2020-09-04 19:34:07 -0400

    first commit

---
+++ PPMToolFullStack/src/main/java/personal/ppmtool/PpmToolFullStackApplication.java
@@ -0,0 +1,13 @@
+package personal.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolFullStackApplication.class, args);
+	}
+
+}
---
+++ PPMToolFullStack/src/test/java/personal/ppmtool/PpmToolFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
